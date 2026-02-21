b12b1b2ebb32f5fce6b83320fa6f2390741bb88a
martinsentis <martin.sentis@gmail.com>
2026-02-21 23:00:40 +0100

    Remove node_modules and dist from repository

--- Backend/dist/services/debt.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Debt } from "../domain/debt.model";
-import { DebtSchedule } from "../domain/debt-schedule.model";
-export declare class DebtService {
-    /**
-     * Génère le tableau d’amortissement mensuel complet
-     */
-    generateSchedule(debt: Debt): DebtSchedule;
-    private addMonths;
-}
