575bf079a3e7ee0b6919eb6bf20f4ce34c73173d
martinsentis <martin.sentis@gmail.com>
2026-03-12 21:17:59 +0100

    changement index

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,19 +1,23 @@
-
-import { DEFAULT_TAX_SCHEDULES } from "../Core/engine/taxEngine";
 import express from "express";
-import { runProjection } from "../Core/engine/projectionEngine";
 import cors from "cors";
+import { runProjection } from "../Core/engine/projectionEngine";
+
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
 
 app.post("/run-projection", (req, res) => {
   try {
     const inputs = req.body;
+
+    console.log("INPUTS RECEIVED:", inputs);
+
     const result = runProjection(inputs);
+
     res.json(result);
   } catch (e) {
-    console.error(e);
+    console.error("ENGINE ERROR:", e);
     res.status(500).json({ error: String(e) });
   }
 });
